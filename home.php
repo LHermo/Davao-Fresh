3538441531e42615171692e2448a4009857d21f2
LHermo <l.hermo.519887@umindanao.edu.ph>
2023-03-30 02:37:14 +0800

    Revised homepage code

--- home.php
+++ home.php
@@ -6,8 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="style.css">
 
+    <link rel="icon" href="assets/icon.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700;900&display=swap" rel="stylesheet">
@@ -16,75 +17,97 @@
 </head>
 
 <body>
-    <div class="hero">
-        <!-- NAVBAR -->
-        <nav>
+    <div class="main-content">
+        <!-- NAVIGATION BAR -->
+        <nav class="nav">
             <img class="logo" src="assets/LOGO - Davao Fresh.svg"></img>
-            <ul>
-                <li><a href="home.php" style="font-weight: 400;"> Home </a></li>
-                <li><a href="fruits.php"> Fruits </a></li>
-                <li><a href="#"> Shopping Basket </a></li>
-                <li><a href="search.php"> Search </a></li>
-                <li><a href="about.php"> About </a></li>
+            <ul style="display: inline-block;">
+                <li class="active"><a href="home.php"> Home </a></li>
+                <li><a href="products"> Products </a></li>
+                <li><a href="about.php"> About Us</a></li>
             </ul>
             <ul>
-                <li><a href="#" style="font-weight: 700;"> Login</a></li>
+                <li><a href="basket.php"><img class="icon" src="assets/shopping-basket.svg" alt="Shopping Basket"></a>
+                </li>
+                <li><a href="login.php"><img class="icon" src="assets/user.svg" alt="Login"></a></li>
             </ul>
-            <!-- <button type="button">Login / Sign Up</button> -->
-            <!-- <ul>
-                <li style="padding: 5px;"><a href="#"><img src="assets/basket-icon.svg"></a></li>
-                <li><a href="#"><img src="assets/person-icon.svg"></a></li>
-            </ul> -->
         </nav>
-        <div class="main-content">
-            <div class="flex-child-element" style="padding-left: 10%; justify-content: center;">
-                <div style="padding-top: 120px;">
-                    <h1> Make a fresh</h1>
-                    <h1> food delivery</h1>
+
+        <!-- THE CONTENT -->
+        <div class="hero">
+            <div class="panel left">
+                <div class="title">
+                    <h1>Make a fresh</h1>
+                    <h1>food delivery</h1>
                 </div>
-                <div style="padding-top: 50px; padding-bottom: 40px; line-height: auto;">
-                    <p style="font-size: 1.1rem">Online shopping from a great selection at Davao Fresh Store. Pick your
-                        basket now and start
+                <div class="home-paragraph">
+                    <p>Online shopping from a great selection at Davao Fresh Store. Pick
+                        your basket now and start
                         shopping!</p>
                 </div>
                 <div>
-                    <span>
-                        <button class="colored-button" style="margin-right: 18px;">Start Shopping</button>
-                        <!-- Tarungon ni, informal ning margin-right-->
-                        <button class="bordered-button"> Sign Up </button>
-                    </span>
+                    <button class="spaced colored-button"> Start Shopping</button>
+                    <button class="bordered-button"> Sign Up</button>
                 </div>
             </div>
-
-            <!-- RIGHT PANEL NA THIS -->
-            <div class=" flex-child-element" style="clear: right;">
-                <img class="wrapper mySlides w3-animate-right" src="assets/banana-only.png" style="z-index: 1;">
-                <img src="assets/ellipse.png" style="position: absolute; top: 1; right: 0;">
+            <div class="panel right">
+                <div style="position: relative; clear: right;">
+                    <img class="wrapper mySlides w3-animate-right banana" src="assets/banana-only.png">
+                    <img class="banana-bg" src="assets/ellipse.png">
+                </div>
             </div>
         </div>
 
         <!-- FOOTER -->
-        <div class=" footer">
-            <p> Developed by Libby Marowen Hermo and Ma. Cristine Joy Comajes</p>
-        </div>
     </div>
-    </div>
-
-    <script>
-        var myIndex = 0;
-        carousel();
-
-        function carousel() {
-            var i;
-            var x = document.getElementsByClassName("mySlides");
-            for (i = 0; i < x.length; i++) {
-                x[i].style.display = "none";
-            }
-            myIndex++;
-            if (myIndex > x.length) { myIndex = 1 }
-            x[myIndex - 1].style.display = "block";
-        }
-    </script>
+    <footer class="footer navbar-fixed-bottom">
+        <p>Developed by </p>
+        <p><a href="https://www.facebook.com/libby.hermo" target="_blank">Libby Marowen D. Hermo</a></p>
+        <p>and </p>
+        <p><a href="https://www.facebook.com/cristine.albisocomajes.3" target="_blank">Ma. Cristine Joy
+                Comajes</a></p>
+    </footer>
 </body>
+
+<script>
+    // Sa Navbar animations ni
+    window.addEventListener('scroll', function () {
+        var navbar = document.querySelector('nav');
+        if (window.pageYOffset > 0) {
+            navbar.classList.add('nav-shadow');
+        } else {
+            navbar.classList.remove('nav-shadow');
+        }
+    });
+
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+            document.querySelector("nav").style.padding = "1% 10%";
+            document.querySelector("nav").style.height = "50px";
+            document.querySelector("nav").style.transition = "all 0.3s ease-in-out";
+        } else {
+            document.querySelector("nav").style.padding = "2% 10%";
+            document.querySelector("nav").style.height = "60px";
+            document.querySelector("nav").style.transition = "all 0.3s ease-in-out";
+        }
+    }
+
+    // Sa Banana Animations ni 
+    var myIndex = 0;
+    carousel();
+
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        myIndex++;
+        if (myIndex > x.length) { myIndex = 1 }
+        x[myIndex - 1].style.display = "block";
+    }
+</script>
 
 </html>
