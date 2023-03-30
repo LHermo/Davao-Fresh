e674a1a9d9ffe108cd5e4bf0a10d09e35f532cd0
LHermo <l.hermo.519887@umindanao.edu.ph>
2023-03-30 21:54:47 +0800

    Created products.html for products page testing

--- home.php
+++ home.php
@@ -23,8 +23,8 @@
             <img class="logo" src="assets/LOGO - Davao Fresh.svg"></img>
             <ul style="display: inline-block;">
                 <li class="active"><a href="home.php"> Home </a></li>
-                <li><a href="products"> Products </a></li>
-                <li><a href="about.html"> About Us</a></li>
+                <li><a href="products.html"> Products </a></li>
+                <li><a href="about.php"> About Us</a></li>
             </ul>
             <ul>
                 <li><a href="basket.php"><img class="icon" src="assets/shopping-basket.svg" alt="Shopping Basket"></a>
@@ -57,9 +57,8 @@
                 </div>
             </div>
         </div>
-
-        <!-- FOOTER -->
     </div>
+    <!-- FOOTER -->
     <footer class="footer navbar-fixed-bottom">
         <p>Developed by </p>
         <p><a href="https://www.facebook.com/libby.hermo" target="_blank">Libby Marowen D. Hermo</a></p>
