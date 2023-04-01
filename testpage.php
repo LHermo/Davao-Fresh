c9da2b25ca620f43cfde1d8ed887b57227cfe37b
LHermo <l.hermo.519887@umindanao.edu.ph>
2023-04-02 03:13:45 +0800

    Created adm_products with sidebar

--- testpage.php
+++ testpage.php
@@ -4,61 +4,66 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+  <link rel="icon" href="assets/icon-green.svg">
+  <title>Manage Products</title>
 </head>
 
-<body style="font-family: 'Montserrat',sans-serif;">
+<body class="bg-light" style="font-family: 'Montserrat',sans-serif;">
   <div class="container-fluid">
     <div class="row flex-nowrap">
-      <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0" style="background: #58A65C;">
+      <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0" style="background: #295F3E">
         <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
           <a href="#" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-            <span class="fs-5 d-none d-sm-inline"><img src="assets/LOGO - Davao Fresh.svg" alt=""></span>
+            <span class="fs-5 d-none d-sm-inline" style="margin: 20px 0px;"><img style="width: 180px;"
+                src="assets/LOGO - Davao Fresh Dark.svg" alt=""></span>
           </a>
-          <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+          <ul style="margin-top: 30px;"
+            class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
             <li class="nav-item">
-              <a href="#" class="nav-link align-middle px-0">
-                <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Products</span>
+              <a href="#" class="nav-link d-flex align-items-center px-0 text-white">
+                <i class="material-icons text-white">inventory</i><span class="ms-1 d-none d-sm-inline"
+                  style="padding-left: 10px;">Products</span></a>
               </a>
             </li>
             <li>
-              <a href="#" class="nav-link px-0 align-middle">
-                <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Customers</span></a>
+              <a href="#" class="nav-link px-0 d-flex align-items-center">
+                <i class="material-icons text-white">diversity_3</i><span class="ms-1 d-none d-sm-inline text-white "
+                  style="padding-left: 10px;">Customers</span></a>
             </li>
             <li>
-              <a href="#" class="nav-link px-0 align-middle">
-                <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Orders</span> </a>
+              <a href="#" class="nav-link px-0 d-flex align-items-center">
+                <i class="material-icons text-white">receipt_long</i><span class="ms-1 d-none d-sm-inline text-white"
+                  style="padding-left: 10px;">Orders</span>
+              </a>
             </li>
             <li>
-              <a href="#" class="nav-link px-0 align-middle">
-                <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Reports</span> </a>
+              <a href="#" class="nav-link px-0 d-flex align-items-center">
+                <i class="material-icons text-white">query_stats</i><span class="ms-1 d-none d-sm-inline text-white"
+                  style="padding-left: 10px;">Reports</span>
+              </a>
             </li>
           </ul>
           <hr>
           <ul class="nav nav=pills flex-column">
-            <li>
-              <a href="#" class="nav-link px-0 align-middle">
-                <span class="material-symbols-outlined">
-                  logout
-                </span>
+            <li class="text-align-middle">
+              <a href="#" class="nav-link px-0 d-flex align-items-center" style="margin-bottom: 20px;">
+                <i class="material-icons text-white">logout</i><span class="ms-1 d-none d-sm-inline text-white"
+                  style="padding-left: 10px;">Logout</span>
+              </a>
+            </li>
             </li>
           </ul>
         </div>
       </div>
-      <div class="col py-3">
-        <h3>Left Sidebar with Submenus</h3>
-        <p class="lead">
-          An example 2-level sidebar with collasible menu items. The menu functions like an "accordion" where only a
-          single
-          menu is be open at a time. While the sidebar itself is not toggle-able, it does responsively shrink in width
-          on
-          smaller screens.</p>
-        <ul class="list-unstyled">
-          <li>
-            <h5>Responsive</h5> shrinks in width, hides text labels and collapses to icons only on mobile
-          </li>
-        </ul>
+
+      <div class="col py-3 bg-white m-4 p-5 rounded shadow-sm">
+        <div class="p-3">
+          <h4 class="fw-bold">Products</h4>
+          <p class="small text-muted">
+            Our fresh produce products table lists all our fruits and vegetables, with categories, descriptions, prices,
+            and stock quantities. You can edit the information and sort/filter products to find what you need quickly.
+            Thanks for your hard work!</p>
+        </div>
       </div>
     </div>
   </div>
