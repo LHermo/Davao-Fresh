6fdf0b249a54f90ee23136df8905a1475eba2645
LHermo <l.hermo.519887@umindanao.edu.ph>
2023-04-11 20:29:54 +0800

    Restored adm_products lost search code

--- adm_products.php
+++ adm_products.php
@@ -1,9 +1,9 @@
 <?php
 include 'conn.php';
-// include 'adm_navbar.php';
+include 'functions.php';
 
+// sa pagination ni na code
 $results_per_page = 6;
-
 // Retrieve total number of results from the database
 $sql = "SELECT COUNT(*) AS count FROM ProductTbl";
 $stmt = $conn->prepare($sql);
@@ -20,22 +20,22 @@ if (!isset($_GET['page'])) {
 } else {
   $current_page = $_GET['page'];
 }
-
 // Calculate the starting and ending positions of the results on the current page
 $start = ($current_page - 1) * $results_per_page;
 $end = $start + $results_per_page;
 
-// Data retrieval sa current page
-$sql = "SELECT * FROM ProductTbl LIMIT $start, $results_per_page";
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$query = "SELECT * FROM ProductTbl
+    WHERE prd_id LIKE '%$searchTerm%'
+    OR prd_name LIKE '%$searchTerm%'
+    OR prd_cat LIKE '%$searchTerm%'
+    OR prd_price LIKE '%$searchTerm%'
+    OR prd_unit LIKE '%$searchTerm%'
+    LIMIT $start, $results_per_page";
 ?>
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <link rel="stylesheet" href="">
@@ -44,21 +44,23 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <title>Products</title>
 </head>
 
+
 <body class="bg-light min-height-100">
   <div class="container-fluid">
     <div class="row flex-nowrap">
+
       <!-- Side bar code starts here -->
       <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0" style="background: #214D34; min-height: 870px;">
-        <!-- <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark"> -->
         <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+
           <!-- Si Logo -->
           <a href="adm_products.php" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
             <span class="fs-5 d-none d-sm-inline" style="margin: 20px 0px;"><img style="width: 180px;" src="assets/LOGO - Davao Fresh Dark.svg" alt=""></span>
           </a>
-          <!-- Si Menu -->
-          <ul style="margin-top: 30px; width: 100%;" class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+          <!-- Si Menu tabs -->
+          <ul style="margin-top: 30px; width: 100%;" class="active nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
             <li class="nav-item w-100 change-when-hovered">
-              <a href="adm_products.php" class="active nav-link d-flex align-items-center px-2 text-white">
+              <a href="adm_products.php" class="nav-link d-flex align-items-center px-2 text-white">
                 <i class="material-icons text-white">inventory</i><span class="ms-1 d-none d-sm-inline" style="padding-left: 10px;">Products</span>
               </a>
             </li>
@@ -78,6 +80,7 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
             </li>
           </ul>
           <hr>
+
           <!-- Si Logout -->
           <ul class="nav nav=pills flex-column w-100">
             <li class="text-align-middle change-when-hovered">
@@ -88,120 +91,123 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
             </li>
           </ul>
         </div>
-      </div> <!-- end ni sidebar code -->
+      </div>
 
       <!-- Content na ni diri -->
       <div class="col py-3 bg-white m-4 p-5 rounded shadow-sm">
+
+        <!-- Table Title + Description -->
         <div class="p-2">
-          <h4 class="fw-bold">Products</h4>
+          <h4 class="fw-bold">Orders</h4>
           <p class="small text-muted">
-            Our fresh produce products table lists all our fruits and vegetables, with categories, descriptions, prices,
-            and stock quantities. You can edit the information and sort/filter products to find what you need quickly.
+            Find and search orders and their details on the table below.
             Thanks for your hard work!</p>
         </div>
 
+        <!-- Table Container -->
         <div style="border: 1.5px solid #DFE2E5; border-radius: 10px;">
 
-          <!-- ABOVE TABLE -->
           <div class="container bg-light" style="border-bottom: 1.5px solid #DFE2E5;">
-            <div class="row align-items-center p-3">
-              <!-- Button -->
-              <div class="add-product-button col-md-3">
-                <button class="adm-table-button"> Add New Product</button>
-              </div>
-              <!-- <div class="col-md-1"></div> -->
-              <!-- Searchbar -->
-              <!-- <div class="searchbar col-md-7">
-                <div class="input-group rounded">
-                  <input type="search" class="form-control rounded" style="width: 300px;" placeholder="Search for products (e.g. Broccoli)" aria-label="Search" aria-describedby="search-addon" />
+            <!-- Search bar -->
+            <form method="post">
+              <div class="row align-items-center p-3">
+                <!-- Button -->
+                <div class="add-product-button col-md-3">
+                  <button class="adm-table-button"> Add New Product</button>
                 </div>
-              </div> -->
-              <div class="input-group" style="max-width: 550px;">
-                <input type="text" class="form-control" placeholder="Search orders here (e.g. Karen Smith)">
-                <div class="input-group-append">
-                  <button class="btn btn-primary bg-success" style="border: none;">
-                    <i class="fas fa-search"></i>
-                  </button>
+                <div class="input-group" style="max-width: 550px;">
+                  <input type="text" name="searchTerm" id="searchTerm" class="form-control" placeholder="Search orders here (e.g. Karen Smith)">
+                  <div class="input-group-append">
+                    <button type="submit" name="search" value="Search" class="btn btn-primary bg-success" style="border: none;">
+                      <i class="fas fa-search"></i>
+                    </button>
+                  </div>
                 </div>
-              </div>
 
-              <!-- Pagination -->
-              <div class="col-md-2">
-                <ul class="pagination  m-0">
-                  <?php
-                  for ($page = 1; $page <= $total_pages; $page++) {
-                    if ($page == $current_page) {
-                      echo '<li class="page-item active"><a class="page-link" href="#">' . $page . '</a></li>';
-                    } else {
-                      echo '<li class="page-item"><a class="page-link" href="?page=' . $page . '">' . $page . '</a></li>';
+                <!-- Pagination -->
+                <div class="col-md-2">
+                  <ul class="pagination  m-0">
+                    <?php
+                    for ($page = 1; $page <= $total_pages; $page++) {
+                      if ($page == $current_page) {
+                        echo '<li class="page-item active"><a class="page-link" href="#">' . $page . '</a></li>';
+                      } else {
+                        echo '<li class="page-item"><a class="page-link" href="?page=' . $page . '">' . $page . '</a></li>';
+                      }
                     }
-                  }
-                  ?>
-                  <ul>
+                    ?>
+                    <ul>
+                </div>
               </div>
-            </div>
+            </form>
           </div>
-          <div class="my-table table-hover table-striped" style="margin: 30px;">
-            <table class="table align-middle mb-0 bg-white" style="border: 1px solid #DFE2E5;">
-              <thead class="bg-white">
-                <tr>
-                  <th style="padding-left: 50px;">ID</th>
-                  <th>Product</th>
-                  <th>Price</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php
 
-                foreach ($results as $row) {
-                  echo '
-                  <tr>
-                      <td style="width: 15%; padding-left: 50px;">
-                          <p class="fw-bold mb-1"> ' . $row['prd_id'] . '</p>
-                      </td>
-                      <td>
+          <!-- Table Content -->
+          <?php displayProducts(
+            $conn,
+            'getProductTable',
+            "SELECT * FROM ProductTbl LIMIT $start, $results_per_page",
+            $query
+          );
+
+          function getProductTable($conn, $query)
+          {
+            $stmt = $conn->query($query);
+            if ($stmt->rowCount() > 0) { ?>
+
+              <div class="my-table table-hover table-striped" id="orders-table" style="margin: 30px;">
+                <table class="table align-middle mb-0 bg-white" style="border: 1px solid #DFE2E5;">
+                  <thead class="bg-white">
+                    <tr>
+                      <th style="padding-left: 50px;">ID</th>
+                      <th>Product</th>
+                      <th>Price</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php
+                    while ($row = $stmt->fetch()) : ?>
+                      <tr>
+                        <td style="width: 15%; padding-left: 50px;">
+                          <p class="fw-bold mb-1"> <?php echo $row['prd_id']; ?> </p>
+                        </td>
+                        <td>
                           <div class="d-flex align-items-center">
-                              <img src=" ' . $row['prd_img'] . '" style="width: 55px; height: 45px; margin-right: 20px" />
-                              <div class="ms-3">
-                                  <p class="fw-bold mb-1" style="font-weight: 500;">' . $row['prd_name'] . '</p>
-                                  <p class="text-muted mb-0 small">' . $row['prd_cat'] . '</p>
-                              </div>
+                            <img src="<?php echo $row['prd_img']; ?>" style="width: 55px; height: 45px; margin-right: 20px" />
+                            <div class="ms-3">
+                              <p class="fw-bold mb-1" style="font-weight: 500;"><?php echo $row['prd_name']; ?></p>
+                              <p class="text-muted mb-0 small"><?php echo $row['prd_cat']; ?></p>
+                            </div>
                           </div>
-                      </td>
-                      <!-- Price + Unit of Measurement  -->
-                      <td>
+                        </td>
+                        <!-- Price + Unit of Measurement  -->
+                        <td>
                           <!-- <span class="badge badge-success rounded-pill d-inline">Active</span> -->
-                          <p class="fw-bold mb-1" style="font-weight: 500;">₱ ' . $row['prd_price'] . '.00</p>
-                          <p class="text-muted mb-0 small">' . $row['prd_unit'] . '</p>
-                      </td>
-                      <!-- Actions  -->
-                      <td>
+                          <p class="fw-bold mb-1" style="font-weight: 500;">₱ <?php echo $row['prd_price']; ?>.00</p>
+                          <p class="text-muted mb-0 small"><?php echo $row['prd_unit']; ?></p>
+                        </td>
+                        <!-- Actions  -->
+                        <td>
                           <button type="button" class="btn btn-sm btn-outline-primary">Edit</button>
                           <button type="button" class="btn btn-sm btn-outline-danger">Remove</button>
-                      </td>
-                      </td>
-                  </tr>';
-                }
-                ?>
-              </tbody>
-            </table>
-          </div>
+                        </td>
+                        </td>
+                      </tr>
+                    <?php endwhile;
+                  } else { ?>
+                    <tr>
+                      <div class="p-3 text-center">
+                        <td class="mx-auto">No results found.</td>
+                      </div>
+                    </tr>
+                  <?php } ?>
+                  </tbody>
+                </table>
+              </div>
+            <?php } ?>
         </div>
-        <!-- end of products table -->
       </div>
     </div>
   </div>
-  <!-- <footer class="bg-light text-center text-lg-start">
-    <div class="small text-center p-3" style="background-color: #e2fc51;">
-      <span>Developed by </span>
-      <span><a href="https://www.facebook.com/libby.hermo" target="_blank" style="font-weight: 700;">Libby Marowen
-          Hermo</a></span>
-      <span>and </span>
-      <span><a href="https://www.facebook.com/cristine.albisocomajes.3" target="_blank" style="font-weight: 700;"> Ma.
-          Cristine Joy Comajes</a></span>
-    </div>
-  </footer> -->
 </body>
-
-</html>
