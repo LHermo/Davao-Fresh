f340c67667b09e70d1772bae3d4dc5e80a4dd51f
LHermo <l.hermo.519887@umindanao.edu.ph>
2023-04-11 21:22:04 +0800

    Separated modal to modal_forms.php

--- adm_test.php
+++ adm_test.php
@@ -1,6 +1,7 @@
 <?php
 include 'conn.php';
 include 'functions.php';
+include 'modal_forms.php';
 
 // sa pagination ni na code
 $results_per_page = 6;
@@ -213,56 +214,6 @@ $query = "SELECT * FROM ProductTbl
                         <?php } ?>
                 </div>
             </div>
-
-            <!-- Modal Code diri -->
-            <div class="modal fade" id="add-product-modal" tabindex="-1" role="dialog" aria-labelledby="add-product-modal-label" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="add-product-modal-label">Add New Product</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-
-                        <form id="add-product-form" method="post" action="submit-product.php">
-                            <div class="modal-body">
-                                <div class="form-group">
-                                    <label for="product-name">Product Name:</label>
-                                    <input type="text" class="form-control" id="product-name" name="product-name" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="product-price">Price:</label>
-                                    <input type="number" class="form-control" id="product-price" name="product-price" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="product-unit">Product Unit:</label>
-                                    <input type="text" class="form-control" id="product-unit" name="product-unit" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="product-category">Category:</label>
-                                    <select class="form-control" id="product-category" name="product-category" required>
-                                        <option value="">-- Select Category --</option>
-                                        <option value="FRUITS">Fruits</option>
-                                        <option value="VEGETABLES">Vegetables</option>
-                                        <option value="GRAINS">Grains</option>
-                                        <option value="HERBS & SPICES">Herbs & Spices</option>
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <label for="product-image">Image URL:</label>
-                                    <textarea class="form-control" id="product-image" name="product-image" rows="3" required></textarea>
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="submit" class="btn btn-primary">Add Product</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div> <!-- End of Modal Code -->
-
         </div>
     </div>
 </body>
