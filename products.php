46019ad5f69aef603216076d1b7db1708f06b0a0
LHermo <l.hermo.519887@umindanao.edu.ph>
2023-04-21 00:23:39 +0800

    Transferred working product catalog to products

--- products.php
+++ products.php
@@ -1,3 +1,8 @@
+<?php
+include 'conn.php';
+include 'functions.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -43,119 +48,54 @@
             <input class="search-bar" type="text" name="" placeholder="Search products (e.g. Brussel Sprouts)">
         </div>
 
-        <?php displayProducts(
-            $conn,
-            'getProductCard',
-            "SELECT * FROM ProductTbl",
-            $query
-        );
+        <div class="all-products">
 
-        function getProductCard($conn, $query)
-        {
-            $stmt = $conn->query($query);
-            if ($stmt->rowCount() > 0) { ?>
-
-                <div class="my-table table-hover table-striped" id="orders-table" style="margin: 30px;">
-                    <table class="table align-middle mb-0 bg-white" style="border: 1px solid #DFE2E5;">
-                        <thead class="bg-white">
-                            <tr>
-                                <th style="padding-left: 50px;">ID</th>
-                                <th>Product</th>
-                                <th>Price</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            while ($row = $stmt->fetch()) : ?>
-                                <tr>
-                                    <td style="width: 15%; padding-left: 50px;">
-                                        <p class="fw-bold mb-1"> <?php echo $row['prd_id']; ?> </p>
-                                    </td>
-                                    <td>
-                                        <div class="d-flex align-items-center">
-                                            <img src="<?php echo $row['prd_img']; ?>" style="width: 55px; height: 45px; margin-right: 20px" />
-                                            <div class="ms-3">
-                                                <p class="fw-bold mb-1" style="font-weight: 500;"><?php echo $row['prd_name']; ?></p>
-                                                <p class="text-muted mb-0 small"><?php echo $row['prd_cat']; ?></p>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <!-- Price + Unit of Measurement  -->
-                                    <td>
-                                        <p class="fw-bold mb-1" style="font-weight: 500;">₱ <?php echo $row['prd_price']; ?>.00</p>
-                                        <p class="text-muted mb-0 small"><?php echo $row['prd_unit']; ?></p>
-                                    </td>
-                                    <!-- Actions  -->
-                                    <td>
-                                        <form method="POST" action="remove-product.php">
-                                            <button type="button" class="btn btn-sm btn-outline-primary" data-toggle="modal" data-target="#edit-product-modal" data-id="<?php echo $row['prd_id'] ?>" data-name="<?php echo $row['prd_name'] ?>" data-price="<?php echo $row['prd_price'] ?>" data-unit="<?php echo $row['prd_unit'] ?>" data-cat="<?php echo $row['prd_cat'] ?>" data-img="<?php echo $row['prd_img'] ?>">Edit</button>
-                                            <input type="hidden" name="id" value="<?php echo $row['prd_id']; ?>">
-                                            <button type="submit" class="btn btn-sm btn-outline-danger delete-btn" data-confirm="Are you sure you want to delete this product?">Delete</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            <?php endwhile;
-                        } else { ?>
-                            <tr>
-                                <div class="p-3 text-center">
-                                    <td class="mx-auto">No results found.</td>
-                                </div>
-                            </tr>
-                        <?php } ?>
-                        </tbody>
-                    </table>
+            <!-- vegetables -->
+            <div style="margin-bottom: 50px">
+                <div class="categ">
+                    <div class="cat-name">Vegetables</div>
+                    <div class="desc">N PRODUCTS</div>
                 </div>
-            <?php } ?>
+                <?php getCatalog($conn, $category = "VEGETABLES"); ?>
+            </div>
 
-
-            <div class="all-products">
-                <div class="vegetables">
-                    <div class="categ">
-                        <div class="cat-name">Vegetables</div>
-                        <div class="desc">N PRODUCTS</div>
-                    </div>
-                    <div class="cards-row">
-                        <div class="product-card">
-                            <div class="product-card-content">
-                                <div class="price">
-                                    <span class="cost">₱ 10.00</span>
-                                    <span class="description">/ per piece</span>
-                                </div>
-                                <div class="product-image"><img src="assets/products/onion.png"></div>
-                                <div class="product-details">
-                                    <p class="category">CATEGORY</p>
-                                    <p class="name">Product Name</p>
-                                </div>
-                                <div class="quantity-selector">
-                                    <button class="minus-btn">-</button>
-                                    <input class="quantity-input" type="text" min="0" value="0">
-                                    <button class="plus-btn">+</button>
-                                </div>
-                                <button class="button-products">Add to basket</button>
-                            </div>
-                        </div>
-                    </div>
+            <!-- fruits -->
+            <div style="margin-bottom: 50px">
+                <div class="categ">
+                    <div class="cat-name">Fruits</div>
+                    <div class="desc">N PRODUCTS</div>
                 </div>
-                <!-- end of vegetables -->
+                <?php getCatalog($conn, $category = "FRUITS"); ?>
+            </div>
 
-                <div class="fruits"></div>
-                <!-- end of fruits -->
+            <!-- grains -->
+            <div style="margin-bottom: 50px">
+                <div class="categ">
+                    <div class="cat-name">Grains</div>
+                    <div class="desc">N PRODUCTS</div>
+                </div>
+                <?php getCatalog($conn, $category = "GRAINS"); ?>
+            </div>
 
-                <div class="grains"></div>
-                <!-- end of grains -->
-
-                <div class="herbs"></div>
-                <!-- end of herbs -->
-
-                <!-- FOOTER -->
-                <footer class="footer navbar-fixed-bottom">
-                    <p>Developed by </p>
-                    <p><a href="https://www.facebook.com/libby.hermo" target="_blank">Libby Marowen D. Hermo</a></p>
-                    <p>and </p>
-                    <p><a href="https://www.facebook.com/cristine.albisocomajes.3" target="_blank">Ma. Cristine Joy
-                            Comajes</a></p>
-                </footer>
+            <!-- herbs -->
+            <div style="margin-bottom: 50px">
+                <div class="categ">
+                    <div class="cat-name">Herbs</div>
+                    <div class="desc">N PRODUCTS</div>
+                </div>
+                <?php getCatalog($conn, $category = "HERBS AND SPICES"); ?>
+            </div>
+        </div>
+    </div>
+    <div style="margin-top: 150px;"></div>
+    <!-- FOOTER -->
+    <footer class="footer navbar-fixed-bottom">
+        <p>Developed by </p>
+        <p><a href="https://www.facebook.com/libby.hermo" target="_blank">Libby Marowen D. Hermo</a></p>
+        <p>and </p>
+        <p><a href="https://www.facebook.com/cristine.albisocomajes.3" target="_blank">Ma. Cristine Joy
+                Comajes</a></p>
+    </footer>
 </body>
 
 <script>
